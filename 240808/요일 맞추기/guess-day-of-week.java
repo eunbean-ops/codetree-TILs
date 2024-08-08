c50f8ae28e14df2971886d5251d8d7b484e8982c
eunbean-ops <rida1223@naver.com>
2024-08-08 15:10:32 +0900

    Title: 요일 맞추기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240808/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        int m1= sc.nextInt();
+        int d1= sc.nextInt();
+        int m2= sc.nextInt();   int cnt=0;
+        int d2= sc.nextInt();
+        int[]month={31,29,31,30,31,30,31,31,30,31,30,31};
+        String[]left={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+        //달
+        if(m1==m2){
+           cnt+= d2-d1+1;
+        }else{
+            cnt+=month[m1-1]-d1;
+            for(int i=m1+1; i<m2; i++){
+            cnt=cnt+month[i-1];
+        }
+            cnt+=d2;
+        }
+        if(cnt<0){
+            cnt=cnt*-1;
+        }
+        
+    
+       
+        System.out.print(left[cnt%7]);
+    }
+}
