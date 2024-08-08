7246023a621d22f2df045e70fa141ab297f21710
eunbean-ops <rida1223@naver.com>
2024-08-08 16:24:57 +0900

    Title: 2진수로 변환하기, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/2진수로 변환하기/convert-to-binary.java
+++ 240808/2진수로 변환하기/convert-to-binary.java
@@ -4,19 +4,19 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        String a="";
-        String aa="";
+       int[]cc= new int[100]; int cnt=0;
         while(true){
             if(n<2){
-                aa=n+"";
-                a=aa+a;
+                cc[cnt]=n;
                 break;
             }else{
-                aa=(n%2)+"";
-                a=aa+a;
+                cc[cnt]=n%2;
                 n=n/2;
+                cnt++;
             }
         }
-        System.out.print(a);
+        for(int i=cnt; i>=0; i--){
+            System.out.print(cc[i]);
+        }
     }
 }
