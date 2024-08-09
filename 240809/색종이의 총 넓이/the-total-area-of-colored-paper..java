b2f9b8dfec5aa38131570706e929efa91686134e
eunbean-ops <rida1223@naver.com>
2024-08-09 22:05:22 +0900

    Title: 색종이의 총 넓이, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240809/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -6,8 +6,8 @@ public class Main {
         int n= sc.nextInt();
         int[][]paper= new int[201][201];
         for(int i=0; i<n; i++){
-            int x= sc. nextInt();
-            int y= sc.nextInt();
+            int x= sc. nextInt()+100;
+            int y= sc.nextInt()+100;
             for(int j=x; j<x+8; j++){
                 for(int k=y; k<y+8; k++){
                     paper[j][k]++;
