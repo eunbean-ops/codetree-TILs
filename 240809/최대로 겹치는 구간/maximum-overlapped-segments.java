bada62ae2bb4903b82827259b5efb3c39263995f
eunbean-ops <rida1223@naver.com>
2024-08-09 15:33:53 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240809/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240809/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -4,10 +4,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc= new Scanner(System.in);
         int k= sc.nextInt();
-        int []num= new int[200];
+        int []num= new int[250];
         for(int i=0; i<k; i++){
-            int a= sc.nextInt(); int aa= a-1;
-            int b= sc.nextInt(); int bb= b-1;
+            int a= sc.nextInt(); int aa= (a-1)+100;
+            int b= sc.nextInt(); int bb= (b-1)+100;
 
             for(int j=aa; j<=bb; j++){
                 num[j]++;
